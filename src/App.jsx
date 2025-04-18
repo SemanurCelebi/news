16815cdb108479f97397780ea28f45dc2428a9eb
Semanur <semanure38@gmail.com>
2025-04-18 14:31:54 +0200

    deploy try

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -10,10 +8,10 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+            sema
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+            çelebi
         </a>
       </div>
       <h1>Vite + Reacts</h1>
